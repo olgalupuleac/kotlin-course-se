05bd1dcb6584efcb17360bd44807699674d7fd64
Olga Lupuleac <olga.lupuleac@gmail.com>
2018-09-18 21:32:27 +0300

    added tests and docs

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -3,10 +3,28 @@ package ru.hse.spb
 import java.lang.Math.sqrt
 import java.util.*
 
+/**
+ * Class which represents an input for a Solver.
+ * We suppose that start point is (0, 0).
+ * x - x coordinate of the endpoint
+ * y - y coordinate of the endpoint.
+ * vMax - the max speed of the plane (actual speed,
+ * because there is no reason to move slower)
+ * timeOfTheFirstWind - time, which the first blows
+ * uX - x component of the first wind's speed
+ * uY - y component of the first wind's speed
+ * wX - x component of the second wind's speed
+ * wY - y component of the second wind's speed
+ */
 data class Input(val x: Double, val y: Double, val vMax: Double,
                  val timeOfTheFirstWind: Double, val uX: Double,
                  val uY: Double, val wX: Double, val wY: Double)
 
+/**
+ * Reads an input from the command line,
+ * sets a start point to (0, 0) and returns an instance of Input
+ * to be passed to Solver.
+ */
 fun readInput(): Input {
     val scanner = Scanner(System.`in`)
     val startPointX = scanner.nextInt()
@@ -31,18 +49,39 @@ fun readInput(): Input {
     )
 }
 
+/**
+ * Class which takes an input and solves the problem.
+ * http://codeforces.com/contest/590/problem/B
+ */
 class Solver(private val input: Input) {
+    /**
+     * Constants used in binary search.
+     * Calculated according to required precision and
+     * input and time limits.
+     */
     companion object {
         const val LEFT_INITIAL = 0.0
         const val RIGHT_INITIAL = 1e9;
         const val NUM_OF_ITERATIONS = 100;
     }
 
+    /**
+     * Checks if the specified point is reachable in the specified
+     * time if the plane goes with the max speed.
+     */
     private fun isReachable(x: Double, y: Double,
                             time: Double): Boolean {
         return sqrt(x * x + y * y) / input.vMax <= time
     }
 
+    /**
+     * Checks if it is possible to reach the endpoint in the
+     * specified time.
+     *
+     * If the time is less than input.timeOfTheFirstWind,
+     * we should consider only displacement caused by the first wind.
+     * Otherwise, we should consider displacements caused by both winds.
+     */
     private fun isValidTime(time: Double): Boolean {
         if (time < input.timeOfTheFirstWind) {
             val x = input.x - time * input.uX
@@ -57,7 +96,11 @@ class Solver(private val input: Input) {
         return isReachable(x, y, time)
     }
 
-    private fun binarySearch(): Double {
+    /**
+     * Makes a binary search
+     * of the time in which the endpoint can be reached.
+     */
+    fun binarySearch(): Double {
         var left = LEFT_INITIAL;
         var right = RIGHT_INITIAL;
         for (i in 1..NUM_OF_ITERATIONS) {
@@ -70,13 +113,9 @@ class Solver(private val input: Input) {
         }
         return right;
     }
-
-    fun solve() {
-        println(binarySearch())
-    }
 }
 
 fun main(args: Array<String>) {
     val solver = Solver(readInput())
-    solver.solve()
+    println(solver.binarySearch())
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -5,7 +5,40 @@ import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun bothWinds() {
+        val solver = Solver(Input(5.0, 5.0,
+                3.0, 2.0,
+                -1.0, -1.0,
+                -1.0, 0.0))
+        assertEquals(3.729935587093555327, solver.binarySearch(), 1e-6)
+    }
+
+    @Test
+    fun onlyOneWind() {
+        val solver = Solver(Input(0.0, 1000.0,
+                100.0, 1000.0,
+                -50.0, 0.0,
+                50.0, 0.0))
+        assertEquals(11.547005383792516398, solver.binarySearch(), 1e-6)
+    }
+
+    @Test
+    fun zeroAnswer() {
+        val solver = Solver(Input(0.0, 0.0,
+                10.0, 100.0,
+                5.0, 0.0,
+                0.0, 0.0))
+        assertEquals(0.0, solver.binarySearch(), 1e-6)
+    }
+
+    @Test
+    fun bigInput() {
+        val solver = Solver(Input(
+                -20000.0, 20000.0,
+                1000.0, 999.0,
+                0.0, -999.0,
+                999.0, 0.0
+        ))
+        assertEquals(1018.7770495642339483, solver.binarySearch(), 1e-6)
     }
 }
